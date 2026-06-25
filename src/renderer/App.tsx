69aab43268cd834e7c4107885baade2a2038e4f0
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-26 01:05:19 +0800

    Initial Commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+    </div>
+  );
+}
