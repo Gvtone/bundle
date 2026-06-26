820b9a1e3c9872727e72daa5c053df8069f7144c
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-26 22:48:08 +0800

    Set up tailwind

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,9 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import "./theme.css";
 
-const container = document.getElementById("root");
-if (!container) {
-  throw new Error("Root element #root not found in index.html");
-}
-
-createRoot(container).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
