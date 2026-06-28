a96f3de6f32872b3c67995e195313d348b3c02a3
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-29 01:29:46 +0800

    Created button ui

--- src/renderer/components/layout/Sidebar.tsx
+++ src/renderer/components/layout/Sidebar.tsx
@@ -1,9 +1,18 @@
+import { PlusIcon } from "@phosphor-icons/react";
+import Button from "../ui/Button";
+
 function Sidebar() {
   return (
     <aside className="flex flex-col bg-card w-1/6 shrink-0 h-full p-4">
-      <h3 className="text-xs tracking-widest font-semibold text-subtle-foreground">
-        TEMPLATES
-      </h3>
+      <div className="flex flex-col gap-2">
+        <h3 className="text-xs tracking-widest font-semibold text-subtle-foreground">
+          TEMPLATES
+        </h3>
+        <Button fullWidth>
+          <PlusIcon weight="bold" />
+          <span>New Template</span>
+        </Button>
+      </div>
     </aside>
   );
 }
