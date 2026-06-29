24df510a4ecde046220a990d8a60c98cf0370498
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-29 14:53:33 +0800

    Created input ui

--- src/renderer/components/layout/Sidebar.tsx
+++ src/renderer/components/layout/Sidebar.tsx
@@ -1,17 +1,42 @@
-import { PlusIcon } from "@phosphor-icons/react";
+import { MagnifyingGlassIcon, PlusIcon } from "@phosphor-icons/react";
 import Button from "../ui/Button";
+import Input from "../ui/Input";
+import { useEffect, useState } from "react";
 
 function Sidebar() {
+  const [templateSearch, setTemplateSearch] = useState("");
+  const [debouncedTemplateSearch, setDebouncedTemplateSearch] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(
+      () => setDebouncedTemplateSearch(templateSearch),
+      400
+    );
+    return () => clearTimeout(timer);
+  }, [templateSearch]);
+
   return (
     <aside className="flex flex-col bg-card w-1/6 shrink-0 h-full p-4">
       <div className="flex flex-col gap-2">
         <h3 className="text-xs tracking-widest font-semibold text-subtle-foreground">
           TEMPLATES
         </h3>
+
         <Button fullWidth>
           <PlusIcon weight="bold" />
           <span>New Template</span>
         </Button>
+
+        <div className="relative flex-1">
+          <MagnifyingGlassIcon className="text-muted-foreground absolute top-1/2 left-4 -translate-y-1/2" />
+          <Input
+            placeholder="Search templates"
+            value={templateSearch}
+            onChange={e => setTemplateSearch(e.target.value)}
+            className="pl-10"
+            fullWidth
+          />
+        </div>
       </div>
     </aside>
   );
---
+++ src/renderer/components/ui/Input.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/renderer/utils/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const inputVariants = cva(
+  "w-fit inline-flex items-center rounded-lg font-medium transition-all duration-200 disabled:opacity-50 disabled:pointer-events-none disabled:cursor-not-allowed whitespace-nowrap",
+  {
+    variants: {
+      variant: {
+        primary: ["bg-card-muted border border-border"],
+        secondary: ["bg-card border border-border"]
+      },
+      scale: {
+        sm: "px-4 py-1 text-sm",
+        md: "px-5 py-2.5 text-sm",
+        lg: "px-6 py-3 text-base"
+      },
+      fullWidth: {
+        true: "w-full"
+      }
+    },
+    defaultVariants: {
+      variant: "primary",
+      scale: "md"
+    }
+  }
+);
+
+interface InputProps
+  extends
+    React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
+
+function Input({
+  className,
+  variant,
+  scale,
+  fullWidth,
+  type = "text",
+  ...props
+}: InputProps) {
+  return (
+    <input
+      type={type}
+      className={cn(inputVariants({ variant, scale, fullWidth }), className)}
+      {...props}
+    />
+  );
+}
+
+export default Input;
