b607aa3fa58a8b91db5aefd11fb81b7b7f0c6e26
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-06-30 22:01:24 +0800

    Created LinkButton component

---
+++ src/renderer/components/ui/LinkButton.tsx
@@ -0,0 +1,79 @@
+import { cn } from "@/renderer/utils/utils";
+import { cva, VariantProps } from "class-variance-authority";
+import { LinkProps, NavLink, NavLinkProps } from "react-router";
+
+const linkButtonVariants = cva(
+  "w-fit inline-flex items-center gap-2 rounded-lg font-medium transition-all duration-200 cursor-pointer disabled:opacity-50 disabled:pointer-events-none disabled:cursor-not-allowed whitespace-nowrap",
+  {
+    variants: {
+      variant: {
+        primary: [
+          "bg-primary text-primary-foreground",
+          "hover:bg-primary-hover"
+        ],
+        secondary: ["bg-card text-foreground", "border border-border"],
+        tertiary: ["bg-transparent", "hover:bg-border"],
+        muted: ["bg-card-muted border border-border"]
+      },
+      size: {
+        sm: "px-4 py-1 text-sm",
+        md: "px-5 py-2.5 text-sm",
+        lg: "px-6 py-3 text-base",
+        icon: "size-7"
+      },
+      contentPosition: {
+        start: "justify-start",
+        center: "justify-center",
+        end: "justify-end",
+        between: "justify-between"
+      },
+      fullWidth: {
+        true: "w-full"
+      },
+      active: {
+        true: "bg-primary-soft"
+      }
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+      contentPosition: "center"
+    }
+  }
+);
+
+interface LinkButtonProps
+  extends NavLinkProps, VariantProps<typeof linkButtonVariants> {}
+
+function LinkButton({
+  to,
+  children,
+  className,
+  variant,
+  size,
+  contentPosition,
+  fullWidth,
+  active,
+  ...props
+}: LinkButtonProps) {
+  return (
+    <NavLink
+      to={to}
+      className={cn(
+        linkButtonVariants({
+          variant,
+          size,
+          contentPosition,
+          active,
+          fullWidth
+        }),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </NavLink>
+  );
+}
+
+export default LinkButton;
