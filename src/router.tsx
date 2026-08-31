cf95b449f07cc411e5543e1cf022df97d36394f5
Gvtone <geraldtolentino82@yahoo.com.ph>
2026-08-31 23:10:01 +0800

    Root page now show CTA

--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,14 @@ import RootLayout from "./renderer/components/layout/RootLayout";
 import EditTemplatePage from "./renderer/pages/EditTemplatePage";
 import DocumentLayout from "./renderer/components/layout/DocumentLayout";
 import FillAndPreviewPage from "./renderer/pages/FillAndPreviewPage";
+import EmptyStatePage from "./renderer/pages/EmptyStatePage";
 
 export const router = createMemoryRouter([
   {
     path: "/",
     Component: RootLayout,
     children: [
+      { index: true, element: <EmptyStatePage /> },
       {
         path: "templates/:templateId",
         Component: DocumentLayout,
